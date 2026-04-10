5090ed0a056e0247725434785f7fbd2000964c64
Ian Bangcoy <ibangcoy@my.centennialcollege.ca>
2026-04-10 17:44:48 -0400

    Add project files.

---
+++ StringExtensionsLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUppercase(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
